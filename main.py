451a536ac20540b9d1d8c84252455bebf7f94818
Higara <Higarayk@gmail.com>
2026-05-22 10:29:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+
+
+
+'''1º o print'''
+
+
+
+#Extrair resultados para o console 
+
+print('---')
+print("Olá")
+print('Mundo')
+
+
+
+'''2º Adicionando comentários'''
+
+
+
+#Comentários não podem ser executados pelo programa
+
+#Comentário de uma linha é usado == ( # )
+
+print('---')
+print('Sexta')
+
+
+'''
+Comentário longo
+é usado == 3 aspas no começo e 3 aspas no final 
+'''
+
+
+
+'''3º Strings e Numeros'''
+
+
+#Data Types 
+
+print('---')
+#   Texto comum é lido como Str (String)  'Texto'
+print('texto')
+
+#   Numeros sem fração são numeros inteiros, lido como Int (Integer) 1,2,3...
+print(123)
+
+#   Numeros fracionados são lidos como (float) 1.5,2.0,3.3...
+print(1.1)
+
+#   Booleano é um tipo de dado primitivo da computação que possui apenas um de dois valores possíveis: Verdadeiro ou Falso
+print(True)
+print(False)
+
+
+
+'''4º Entendendo sobre variáveis'''
+
+
+
+
+
